4164cf683d4cded650c668d77a92f56ee7ccfd7c
CodyAdcock <codyadcock10@gmail.com>
2021-05-07 13:20:58 -0600

    files set up and ready to start

---
+++ ExpenseTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Cody Adcock on 5/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
